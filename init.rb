27607ae916de0e4bf2b5b792578969bb79d83cd2
Ken Robertson <ken@invalidlogic.com>
2011-05-13 13:56:18 -0700

    Dont load read/write splitting when running rake or when in development/test mode

--- init.rb
+++ init.rb
@@ -1,3 +1,3 @@
-if RUBY_PLATFORM =~ /java/ && ENV['NODB'].nil?
+if RUBY_PLATFORM =~ /java/ && ENV['NODB'].nil? && $0 !~ /rake/ && !["development", "test"].include?(Rails.env)
   require 'active_record_jdbc_mysql_master_slave'
 end
